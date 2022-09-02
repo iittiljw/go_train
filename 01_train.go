7ae7dd2a2fbd7e71fafae6986f541f17aa65a917
iittiljw <iittiljw@126.com>
2022-09-02 16:24:59 +0800

    add file

--- 01_train.go
+++ 01_train.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 )
 
+/*
 const (
+
 	asa = iota
 	b   = 1 << (iota * 10)
 	c   = 1 << (iota * 10)
@@ -12,20 +14,46 @@ const (
 	e   = 1 << (iota * 10)
 	f   = 1 << (iota * 10)
 	g   = 1 << (iota * 10)
+
 )
 
-func Map2String(m map[string]string) (ret string) {
-	for k, v := range m {
-		ret += fmt.Sprintf("%s=%s\n", k, v)
+	func Map2String(m map[string]string) (ret string) {
+		for k, v := range m {
+			ret += fmt.Sprintf("%s=%s\n", k, v)
+		}
+		return
 	}
-	return
-}
+*/
 func main() {
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Println(e)
-	fmt.Println(f)
-	fmt.Println(g)
+	//	a := [...]int{5, 3, 2, 5, 12}
+	//	fmt.Println(a)
+	m := map[int]string{1: "OK", 2: "AA", 3: "BB", 4: "CC", 5: "DD"}
+
+	//m["OK"] = 11
+	for _, v := range m {
+		fmt.Println(v)
+	}
+	fmt.Println(m)
+
+	/*
+		num := len(a)
+		for i := 0; i < num; i++ {
+			for j := i + 1; j < num; j++ {
+				if a[i] < a[j] {
+					temp := a[i]
+					a[i] = a[j]
+					a[j] = temp
+				}
+
+			}
+		}
+		fmt.Println(a)
+	*/
+	//fmt.Println(b)
+	//	fmt.Println(c)
+	//	fmt.Println(d)
+	//	fmt.Println(e)
+	//	fmt.Println(f)
+	//	fmt.Println(g)
 
 }
